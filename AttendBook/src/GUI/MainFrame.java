2258b4283520daa5fd805f729a4f86c319bfb01c
User <User@User-PC>
2019-11-08 17:58:08 +0900

    Setting the base of Classes and Packes.

--- AttendBook/src/GUI/MainFrame.java
+++ AttendBook/src/GUI/MainFrame.java
@@ -1,10 +1,21 @@
 package GUI;
 
-public class MainFrame {
+import javax.swing.JFrame;
 
+public class MainFrame extends JFrame{
+
+	public MainFrame() {
+		Showing();
+	}
+	public void Showing() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(960,640);
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new MainFrame();
 	}
 
 }
