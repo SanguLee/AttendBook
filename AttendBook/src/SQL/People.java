c53aed4b6fe13097921a08848e8b5807d72fc134
User <User@User-PC>
2019-11-11 12:49:03 +0900

    Just i make for look easy. i wll delete it lator maybe.

---
+++ AttendBook/src/SQL/People.java
@@ -0,0 +1,17 @@
+package SQL;
+
+public class People {
+	public static final int ID = 0;
+	public static final int NAME = 0;
+	public static final int CLASS = 0;
+	public static final int SEX = 0;
+	public static final int PHONE = 0;
+	public static final int BIRTH = 0;
+	public static final int ADDRESS = 0;
+	
+	public static final String[] type = {"ID", "NAME", "CLASS", "SEX", "PHONE", "BIRTH", "ADDRESS"};
+	
+	public enum type{
+		ID,NAME,CLASS,SEX,PHONE,BIRTH,ADDRESS
+	}
+}
