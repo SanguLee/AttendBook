fd82fbb4c68a68b3fee876b2cdf18e1712cb4e42
User <User@User-PC>
2019-11-13 17:28:07 +0900

    make serialID, constructor

--- AttendBook/src/GUI/Contents/Content.java
+++ AttendBook/src/GUI/Contents/Content.java
@@ -1,5 +1,11 @@
 package GUI.Contents;
 
-public class Content {
+import GUI.Setting.Panel;
 
+public class Content extends Panel {
+	private static final long serialVersionUID = -1801618486491267486L;
+
+	public Content() {
+		this.setBackground(Panel.BACK);
+	}
 }
