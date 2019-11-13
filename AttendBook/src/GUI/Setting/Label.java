1ed2d893cad058c30d57c1337769c37b559fedad
User <User@User-PC>
2019-11-13 17:28:41 +0900

    get 'FontName' from styles class

--- AttendBook/src/GUI/Setting/Label.java
+++ AttendBook/src/GUI/Setting/Label.java
@@ -6,17 +6,18 @@ import javax.swing.JLabel;
 
 public class Label extends JLabel{
 	private static final long serialVersionUID = 7036064893078447973L;
+	public final static String FONTNAME = " Noto Sans CJK KR";
 	public enum type{
 		TITLE, BACK
 	};
 	public Label(type t, int size) {
 		switch (t) {
 		case TITLE:	
-			this.setFont(new Font(Styles.FONTNAME, Font.BOLD, size)); 
+			this.setFont(new Font(FONTNAME, Font.BOLD, size)); 
 			this.setForeground(Styles.PRIMEFONT);
 			break;
 		case BACK : 
-			this.setFont(new Font(Styles.FONTNAME, Font.PLAIN, size)); 
+			this.setFont(new Font(FONTNAME, Font.PLAIN, size)); 
 			this.setForeground(Styles.BACKFONT);
 			break;
 		}
