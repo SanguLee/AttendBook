14e9bc5aecb48da5382d812239ebb5b61d6ec91e
User <User@User-PC>
2019-11-14 17:47:26 +0900

    I make misstake. i dont need to set layout of head panel 'twice'

--- AttendBook/src/GUI/Contents/Content.java
+++ AttendBook/src/GUI/Contents/Content.java
@@ -1,11 +1,33 @@
 package GUI.Contents;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import GUI.Setting.Panel;
 
-public class Content extends Panel {
+public abstract class Content extends Panel {
 	private static final long serialVersionUID = -1801618486491267486L;
+
+	protected Panel head = new Panel();
+	protected Panel body = new Panel();
 
 	public Content() {
 		this.setBackground(Panel.BACK);
+		this.setLayout(new BorderLayout());
+
+		head.setPreferredSize(new Dimension(0, 120));
+
+		this.add(head, BorderLayout.NORTH);
+		this.add(body, BorderLayout.CENTER);
+
+		head.setLayout(new BorderLayout());
+		body.setLayout(new BorderLayout());
+
+		setHead();
+		setBody();
 	}
+
+	abstract void setHead();
+
+	abstract void setBody();
 }
