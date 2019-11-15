e335e087cc865dca82626d4b9223e8ce677d75b3
User <User@User-PC>
2019-11-15 17:38:21 +0900

    make Attend class to clearly

--- AttendBook/src/GUI/Contents/AttendPanel.java
+++ AttendBook/src/GUI/Contents/AttendPanel.java
@@ -1,74 +1,115 @@
 package GUI.Contents;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.border.EmptyBorder;
 
 import GUI.Setting.GridBag;
 import GUI.Setting.Label;
 import GUI.Setting.Panel;
 import GUI.Setting.Styles;
-import GUI.Setting.Label.type;
 
 public class AttendPanel extends Content {
 	private static final long serialVersionUID = -1280465224088539851L;
-	
+
 	@Override
 	void setHead() {
 		// TODO Auto-generated method stub
 		head.add(new JButton("제 ㅡ 목"), BorderLayout.CENTER);
-		System.out.println("실행중");
 	}
 
 	@Override
 	void setBody() {
 		// TODO Auto-generated method stub
-		GridBag grid = new GridBag();
+		GridBag grid = new GridBag(6);
 		JButton[] b = new JButton[7];
+
 		for (int i = 0; i < 7; i++) {
 			b[i] = new JButton(i + "");
+
 			if (i < 5) {
-				grid.addGrid(b[i], i / 4, i % 4, 1, 1);
+				// 1,2,3 학년, 새신자, 교사
+				grid.addGrid(new Attend(i, null), i / 4, i % 4, 1, 1);
 			}
 			if (i == 5) {
 				grid.addGrid(b[i], i / 4, i % 4, 1, 1);
 			}
 			if (i == 6) {
-				grid.addGrid(b[i], i / 4, i % 4, 2, 1);;
+				grid.addGrid(b[i], i / 4, i % 4, 2, 1);
 			}
 			if (i == 7) {
 				// do nothing in here
 			}
 		}
+
 		body.add(grid, BorderLayout.CENTER);
 	}
 }
 
 class Attend extends Panel {
-	Panel Head;
+	private static final long serialVersionUID = 2022117008532120900L;
 
+	Panel Head;
 	Panel Body;
+
 	Panel[] Bodies = new Panel[2];
 
 	public static final String[] title = { "1학년", "2학년", "3학년", "새신자", "교사" };
 	public static final String[] sex = { "남자", "여자" };
 
-	Panel[] cols;
-	Panel[][] rows;
+	Panel[] cols = new Panel[maxcol];
+	Panel[][] rows = new Panel[maxcol][maxrow];
 
+	static final int maxcol = 2;
 	static final int maxrow = 8;
 
-	public Attend(int t) {
-		Head = new Panel(Styles.PRIMARY);
-		this.add(Head, BorderLayout.NORTH);
-		Head.add(new Label(type.NORMAL, title[t]));
+	public Attend(int t, String[] names) {
 
+		// 헤드 ( 타이틀 ) 설정
+		Head = new Panel(Panel.TITLE);
+		this.add(Head, BorderLayout.NORTH);
+
+		Head.add(new Label(Label.PRIMEFONT, Label.SMALL, title[t]));
+
+		// 바디 ( 필드값) 설정
+		Body = new Panel();
+		Body.setLayout(new GridLayout(1, 2));
 		this.add(Body, BorderLayout.CENTER);
 
-		for (int i = 0; i < maxrow; i++) {
-			for (int j = 0; j < 2; j++) {
+		// 바디즈 ( 필드 2개 ) 설정
+		for (int i = 0; i < maxcol; i++) {
+			Bodies[i] = new Panel();
+			Bodies[i].setLayout(new GridLayout(maxrow + 1, 1));
+			Body.add(Bodies[i]);
 
+			// 컬럼 ( 남녀 ) 설정
+			cols[i] = new Panel(Panel.TITLE);
+			cols[i].add(new Label(Label.PRIMEFONT, Label.SMALL, sex[i]));
+
+			Bodies[i].add(cols[i]);
+
+			// 로우 ( 필드 ) 설정
+			for (int j = 0; j < maxrow; j++) {
+				rows[i][j] = new Panel(Panel.WHITE);
+				rows[i][j].setBorder(new EmptyBorder(3, 3, 3, 3));
+
+				if (names!=null &&  j < names.length) {
+					rows[i][j].add(new Label(Label.NORMALFONT, Label.SMALL, names[j]), BorderLayout.CENTER);
+					rows[i][j].add(new JCheckBox(), BorderLayout.EAST);
+				} else
+					rows[i][j].add(new Label(Label.NORMALFONT, Label.SMALL, "\t"), BorderLayout.CENTER);
+
+				Bodies[i].add(rows[i][j]);
 			}
 		}
+	}
+}
+
+class Sum extends Panel {
+	public Sum() {
+
 	}
 }
