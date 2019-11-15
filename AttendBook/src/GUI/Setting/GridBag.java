d5f3f1681c8b0b308ad563ae1cc363b72f9b2d05
User <User@User-PC>
2019-11-15 17:45:35 +0900

    make empty border set from parameter

--- AttendBook/src/GUI/Setting/GridBag.java
+++ AttendBook/src/GUI/Setting/GridBag.java
@@ -1,8 +1,10 @@
 package GUI.Setting;
 
-import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 
 public 
 class GridBag extends Panel{
@@ -10,15 +12,18 @@ class GridBag extends Panel{
 	
 	GridBagLayout grid = new GridBagLayout();
 	GridBagConstraints con = new GridBagConstraints();
+	int b = 0;
 	
-	public GridBag() {
+	public GridBag(int border) {
+		this.b = border;
 		this.setLayout(grid);
 		con.weightx = 1.0;
 		con.weighty = 1.0;
 		con.fill=GridBagConstraints.BOTH;
 	}
 	
-	public void addGrid(Component c, int x, int y, int w, int h) {
+	public void addGrid(JComponent c, int x, int y, int w, int h) {
+		c.setBorder(BorderFactory.createEmptyBorder(b, b, b, b));
 		con.gridx = y;
 		con.gridy = x;
 		con.gridwidth = w;
