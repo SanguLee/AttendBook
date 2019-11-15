b0c0007bd71b8d17e64bf5510153b41be9b1f0d0
User <User@User-PC>
2019-11-15 17:37:39 +0900

    set constructor parameter. not use switch

--- AttendBook/src/GUI/Setting/Label.java
+++ AttendBook/src/GUI/Setting/Label.java
@@ -1,30 +1,31 @@
 package GUI.Setting;
 
+import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JLabel;
 
-public class Label extends JLabel{
+public class Label extends JLabel {
 	private static final long serialVersionUID = 7036064893078447973L;
 	public final static String FONTNAME = " Noto Sans CJK KR";
-	public enum type{
-		TITLE, BACK, NORMAL
-	};
-	public Label(type t, String s) {
-		switch (t) {
-		case TITLE:	
-			this.setFont(new Font(FONTNAME, Font.BOLD, 21)); 
-			this.setForeground(Styles.PRIMEFONT);
-			break;
-		case BACK : 
-			this.setFont(new Font(FONTNAME, Font.PLAIN, 10)); 
-			this.setForeground(Styles.BACKFONT);
-			break;
-		case NORMAL : 
-			this.setFont(new Font(FONTNAME, Font.PLAIN, 10)); 
-			this.setForeground(Styles.NORMALFONT);
-			break;
-		}
-		
+
+	public final static Color PRIMEFONT = Styles.BACKGROUND;
+	public final static Color BACKFONT = Styles.SECONDARY;
+	public final static Color NORMALFONT = Styles.DARKEST;
+
+	public final static int BIG = 21;
+	public final static int SMALL = 12;
+	public final static int TINY = 8;
+
+	public Label(Color color, int size, String text) {
+		// 글자 색상
+		this.setForeground(color);
+
+		// 글자 크기
+
+		this.setFont(new Font(FONTNAME, Font.PLAIN, size));
+
+		// 글자 내부 내용
+		this.setText(text);
 	}
 }
